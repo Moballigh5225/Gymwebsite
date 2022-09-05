16d7453e3247f8f1e72e5a58ffde7e715e178cb7
Moballighul islam <moballigh5225@gmail.com>
2022-09-06 00:52:27 +0530

    added right side

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./hero.css";
 import Header from "../Header/Header";
 
+// images import
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -46,8 +52,24 @@ const Hero = () => {
             <span>fitness programs</span>
           </div>
         </div>
+        {/* hero button */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
       </div>
-      <div className="right-h">right side</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+      </div>
     </div>
   );
 };
