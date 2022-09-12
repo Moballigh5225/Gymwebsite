3a3a15ce42dc66cf84ef98866a05479c0c5ab499
Moballighul islam <moballigh5225@gmail.com>
2022-09-12 23:34:52 +0530

    animation added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./hero.css";
 import Header from "../Header/Header";
+// importing motion from the framer motion
+import { motion } from "framer-motion";
 
 // images import
 import hero_image from "../../assets/hero_image.png";
@@ -9,15 +11,23 @@ import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 
 const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
 
         {/* the best ad structure */}
 
         <div className="the-best-ad">
-          <div></div>
+          {/* used framer motion for animation */}
+          <motion.div
+            initial={{ left: "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
           <span>the best fitness club in the town</span>
         </div>
 
@@ -61,22 +71,39 @@ const Hero = () => {
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div
+          className="heart-rate"
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={transition}
+        >
           <img src={Heart} alt="" />
           <span>Heart Rate</span>
           <span>116 bpm</span>
-        </div>
+        </motion.div>
         {/* hero images */}
         <img src={hero_image} alt="" className="hero-image" />
-        <img src={hero_image_back} alt="" className="hero-image-back" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          src={hero_image_back}
+          alt=""
+          className="hero-image-back"
+        />
         {/* Calories */}
-        <div className="calories">
+        <motion.div
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+          className="calories"
+        >
           <img src={Calories} alt="" />
           <div>
             <span>Calories Burned</span>
             <span>220 kilo</span>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./testimonial.css";
 import { testimonialsData } from "../../data/testimonialsData";
+import { motion } from "framer-motion";
 
 // images import
 
@@ -11,6 +12,7 @@ const Testimonial = () => {
   const [selected, setSelected] = useState(0);
   const tLength = testimonialsData.length;
   console.log(tLength);
+  const transition = { type: "spring", duration: 3 };
 
   return (
     <div className="Testitmonials">
@@ -18,7 +20,15 @@ const Testimonial = () => {
         <span>Testimonials</span>
         <span className="stroke-text">what they</span>
         <span>say about us</span>
-        <span>{testimonialsData[selected].review}</span>
+        <motion.span
+          key={selected}
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 100 }}
+          transition={transition}
+        >
+          {testimonialsData[selected].review}
+        </motion.span>
         <span>
           <span style={{ color: "var(--orange)" }}>
             {testimonialsData[selected].name}
@@ -28,9 +38,25 @@ const Testimonial = () => {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
-        <img src={testimonialsData[selected].image} alt="" />
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ ...transition, duration: 2 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ ...transition, duration: 2 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        ></motion.div>
+        <motion.img
+          key={selected}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={transition}
+          src={testimonialsData[selected].image}
+          alt=""
+        />
         <div className="arrows">
           <img
             onClick={() => {
